79aedee4f43fe5b7c4022f6928324d1d9326cd3f
Amrithmani <amrimani91@gmail.com>
2018-02-16 10:23:32 +0530

    Added comment and made some changes

--- app/src/main/java/com/amritha/acadgild/android_session9_assignment1/MainActivity.java
+++ app/src/main/java/com/amritha/acadgild/android_session9_assignment1/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Definig TextView
     TextView textView;
 
     @Override
@@ -18,18 +19,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Get TextView object from xml
+
         textView = findViewById(R.id.textView);
     }
 
+    //creating options menu method which uses inflater
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
 
-        inflater.inflate(R.menu.submenu, menu);
+        inflater.inflate(R.menu.submenu, menu);//calling submenu.xml
 
         return super.onCreateOptionsMenu(menu);
     }
 
+    //creating options item selected method which uses switch cases
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
@@ -37,15 +42,15 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()) {
 
             case R.id.red:
-                textView.setTextColor(Color.parseColor("#F44336"));
+                textView.setTextColor(Color.parseColor("#F44336"));//changing Text View color to Red
                 break;
 
             case R.id.blue:
-                textView.setTextColor(Color.parseColor("#2196F3"));
+                textView.setTextColor(Color.parseColor("#2196F3"));//changing Text View color to Green
                 break;
 
             case R.id.green:
-                textView.setTextColor(Color.parseColor("#4CAF50"));
+                textView.setTextColor(Color.parseColor("#4CAF50"));//changing Text View color to Blue
                 break;
         }
 
